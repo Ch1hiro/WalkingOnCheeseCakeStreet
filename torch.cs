18e8d5ca08f2e43854a740fe5a64fb8913825e15
SANO, Chihiro <49871764+Ch1hiro@users.noreply.github.com>
2020-05-02 13:43:11 +0900

    Add files via upload
    懐中電灯を持ってうろつけるようになりました。
    矢印キー上下で光の量を調節することができます。

---
+++ torch.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  torch : MonoBehaviour {
+
+  public Light torch;
+  public float switch_vel;
+
+  void Update (){
+
+    if (Input.GetKey(KeyCode.Up)){
+      torch.range += switch_vel;
+    }
+
+    if (Input.GetKey(KeyCode.Down)){
+      torch.range -= switch_vel;
+    }
+
+  }
+}
